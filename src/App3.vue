ca1a155bdd2a30d8d8934ec8feff93ff256f0f1f
Samuel Ezedi <ezedi@Samuels-MacBook-Pro.local>
2024-12-22 14:43:35 +0100

    justed added tailwindcss

---
+++ src/App3.vue
@@ -0,0 +1,67 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const name = ref('John Doe')
+const status = ref('pending')
+const tasks = ref(['Task One', 'Task Two', 'Task Three'])
+const newTask = ref('')
+
+const toggleStatus = () => {
+  if (status.value === "active") {
+    status.value = 'pending'
+  } else if (status.value == "pending") {
+    status.value = 'inactive'
+  } else {
+    status.value = 'active'
+  }
+}
+
+const addTask = () => {
+  if (newTask.value.trim() !== '') {
+    tasks.value.push(newTask.value)
+    newTask.value = ''
+  }
+}
+
+const deleteTask = (index) => {
+  tasks.value.splice(index, 1);
+}
+
+onMounted(async () => {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos')
+    const data = await response.json()
+    tasks.value = data.map((task) => task.title)
+  } catch (e) {
+    console.log('Erro fetching task.')
+  }
+})
+
+</script>
+
+<template>
+  <h1>Hello {{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status == 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add Task</label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask">
+    <button>Save</button>
+  </form>
+
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="(task, index0 in tasks" :key="task">
+      <span>
+        {{ task }}
+      </span>
+      <button @click="deleteTask(index)">
+        x
+      </button>
+    </li>
+  </ul>
+
+  <button @click="toggleStatus">Change status</button>
+</template>
